cb14d0b1c9651600c551ca918e036fbc05ab1ce1
brsaca <brsaca@gmail.com>
2023-09-14 13:12:32 -0600

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Brenda Saavedra Cantu on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
