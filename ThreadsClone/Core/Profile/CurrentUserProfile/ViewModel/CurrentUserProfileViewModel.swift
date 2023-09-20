50a6ec9e8299916330ab352a19d8d8aa1785a6a6
brsaca <brsaca@gmail.com>
2023-09-20 10:29:45 -0600

    fix: navigation to profile in search

--- ThreadsClone/Core/Profile/CurrentUserProfile/ViewModel/CurrentUserProfileViewModel.swift
+++ ThreadsClone/Core/Profile/CurrentUserProfile/ViewModel/CurrentUserProfileViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  ProfileViewModel.swift
+//  CurrentUserProfileViewModel.swift
 //  ThreadsClone
 //
 //  Created by Brenda Saavedra Cantu on 19/09/23.
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-class ProfileViewModel: ObservableObject {
+class CurrentUserProfileViewModel: ObservableObject {
     @Published var currentUser: User?
     private var cancellables = Set<AnyCancellable>()
     
