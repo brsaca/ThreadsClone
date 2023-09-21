dcf3b70fb7669eca60f9470bd6dd02df720e8644
brsaca <brsaca@gmail.com>
2023-09-20 19:25:50 -0600

    feat: load feedlist from each user

---
+++ ThreadsClone/Core/Profile/Components/ViewModel/UserContentListViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  UserContentListViewModel.swift
+//  ThreadsClone
+//
+//  Created by Brenda Saavedra Cantu on 20/09/23.
+//
+
+import Foundation
+
+class UserContentListViewModel: ObservableObject {
+    @Published var threads = [Thread]()
+    
+    let user: User
+    
+    init(user: User){
+        self.user = user
+        Task { try await fetchUserThreads() }
+    }
+    
+    @MainActor
+    func fetchUserThreads() async throws {
+        var threads = try await ThreadService.fetchUserThreads(uid: user.id)
+        
+        for i in 0 ..< threads.count {
+            threads[i].user = self.user
+        }
+        
+        self.threads = threads
+    }
+}
